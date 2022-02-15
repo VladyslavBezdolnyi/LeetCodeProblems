93ba0f1ad7e36da66bcc7fb6c4cf075d7bbcb6f7
Vlad Bezdolniy <spiring110@ukr.net>
2022-02-16 00:25:29 +0200

    two sum

--- src/com/vladyslav/Main.java
+++ src/com/vladyslav/Main.java
@@ -1,13 +1,55 @@
 package com.vladyslav;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Solution sol = new Solution();
+        int[] myResultSet = {2,7,11,15};
+        System.out.println(Arrays.toString(sol.twoSum(myResultSet, 18)));
+
     }
 }
 
 class Solution {
+    
+    /*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.*/
+    public int[] twoSum(int[] nums, int target) { // 2, 7, 11, 15
+
+        int[] resultTwoNumsIndexes = new int[2];
+        for (int i = 0; i < nums.length; i++){
+            for (int j = i + 1; j < nums.length; j++){
+                if (nums[i] + nums[j] == target){
+                    resultTwoNumsIndexes[0] = i;
+                    resultTwoNumsIndexes[1] = j;
+                }
+            }
+        }
+        return resultTwoNumsIndexes;
+
+        /*
+        O(n)
+        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < numbers.length; i++) {
+            if (map.containsKey(target - numbers[i])) {
+                result[1] = i;
+                result[0] = map.get(target - numbers[i]);
+                return result;
+            }
+            map.put(numbers[i], i);
+        }
+        return result;*/
+    }
+
     public int[] getConcatenation(int[] nums) {
 
         int[] ans = new int[nums.length * 2];
