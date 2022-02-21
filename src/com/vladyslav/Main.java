9f0ffed6da21ea4f177e5e9d8e76d4f416a0de1c
Vlad Bezdolniy <spiring110@ukr.net>
2022-02-21 03:02:09 +0200

    Palindrome

--- src/com/vladyslav/Main.java
+++ src/com/vladyslav/Main.java
@@ -1,19 +1,115 @@
 package com.vladyslav;
 
 import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class Main {
 
     public static void main(String[] args) {
         Solution sol = new Solution();
-        int[] myResultSet = {4,1,2,1,2};
-        System.out.println(sol.singleNumber(myResultSet));
-
+        System.out.println(sol.longestPalindrome("xaabacxcabaaxcabaax"));
     }
+
+
 }
 
 class Solution {
 
+
+    /*    Given a string s, return the longest palindromic substring in s.*/
+    public String longestPalindrome(String s) {
+
+        if (s.length() == 1){
+            return s;
+        }
+
+        class PalindromeNode {
+            private final int[] indexes = new int[2];
+
+            PalindromeNode(int firstIndex, int lastIndex){
+                indexes[0] = firstIndex;
+                indexes[1] = lastIndex;
+            }
+
+            public int getFirst(){
+                return indexes[0];
+            }
+
+            public int getLast(){
+                return indexes[1];
+            }
+        }
+
+        Map<String, ArrayList<Integer>> groupByCharAndIndex = new HashMap<>();
+        String[] splitArray = s.split("");
+        TreeMap<Integer, PalindromeNode> finalResultNodes = new TreeMap<>((o1, o2) -> o2 - o1);
+
+        for (int i = 0; i < s.length(); i++){
+            if (groupByCharAndIndex.containsKey(splitArray[i])){
+                groupByCharAndIndex.get(splitArray[i]).add(i);
+            }
+            else {
+                groupByCharAndIndex.put(splitArray[i], new ArrayList<Integer>(List.of(i)));
+            }
+        }
+
+        //change this to priority queue
+        Map<Integer, PalindromeNode> sortedNodes = new TreeMap<>((o1, o2) -> o2 - o1);
+        for (var entry : groupByCharAndIndex.entrySet()) {
+            if (entry.getValue().size() == 1) {
+                continue;
+            }
+            for (int i = 0; i < entry.getValue().size() - 1; i++) {
+                for (int j = i + 1; j < entry.getValue().size(); j++) {
+                    sortedNodes.put(Math.abs(entry.getValue().get(i) - entry.getValue().get(j)) + 1,
+                            new PalindromeNode(entry.getValue().get(i), entry.getValue().get(j)));
+                }
+            }
+        }
+
+        Function<PalindromeNode, Boolean> isPalindrome = (node) -> {
+                for (int i = node.getFirst(), j = node.getLast(); i < j; i++, j--){
+                    if (!splitArray[i].equals(splitArray[j])){
+                        return false;
+                    }
+                }
+                return true;
+            };
+
+        for (var sortedEntry : sortedNodes.entrySet()){
+            if(isPalindrome.apply(sortedEntry.getValue())){
+                finalResultNodes.put(sortedEntry.getKey(), sortedEntry.getValue());
+                break;
+            }
+        }
+
+        if (finalResultNodes.isEmpty()){
+            return splitArray[0];
+        }
+        return s.substring(finalResultNodes.firstEntry().getValue().getFirst(), finalResultNodes.firstEntry().getValue().getLast() + 1);
+    }
+
+   /* Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+    Note that the same word in the dictionary may be reused multiple times in the segmentation.*/
+    public boolean wordBreak(String s, List<String> wordDict) {
+
+        StringBuilder resultString = new StringBuilder(s);
+        for (String value : wordDict){
+            int indexOfContainedValue = resultString.indexOf(value);
+            if(indexOfContainedValue == -1){
+                return false;
+            }
+            else {
+                resultString.delete(indexOfContainedValue, indexOfContainedValue + value.length());
+                if (resultString.length() == 0){return true;}
+            }
+
+        }
+        return resultString.length() == 0 || wordBreak(resultString.toString(), wordDict);
+
+    }
    /* Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
     You must implement a solution with a linear runtime complexity and use only constant extra space.*/
